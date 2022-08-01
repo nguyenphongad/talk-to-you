a67228129ca2ac36ff52143700ea5752c97a056d
talk to you <cuphong2003hk@gmail.com>
2022-08-01 13:02:50 +0700

    TTY

--- project_TTY/src/App.js
+++ project_TTY/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          oke bạn nha
+          oke bạn nha hihi
         </a>
       </header>
     </div>
